fbe24ed8edb4211b719740cec063c65211bfc8a1
on-three <on_three@outlook.com>
2015-08-09 01:44:59 -0700

    simple start script

---
+++ start.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+STREAM_DISPLAY=:1.0
+STREAM_WIDTH=1280
+STREAM_HEIGHT=720
+
+# Start up a Xephyr virtual desktop dedicated to display
+#Xephyr ${STREAM_DISPLAY} -host-cursor -screen ${STREAM_WIDTH}x${STREAM_HEIGHT}x16 -dpi 96 -ac -extension Composite &
+Xephyr ${STREAM_DISPLAY} -host-cursor -screen ${STREAM_WIDTH}x${STREAM_HEIGHT}x16 -ac &
+
+# For now, just move the Xephyr display to a fixed position on the desktop
+# We can easily set vlc to capture this seciton, but in the future we should
+# capture the window via x11 and pipe to vlc
+#xdotool
+
+sleep 1
+
+# Start up our custom Tiling Window Manager on the virtual desktop
+# This allows us to control stream and video window layouts "easily"
+#DISPLAY=${STREAM_DISPLAY} nwm --xephyr -glamor &
+DISPLAY=${STREAM_DISPLAY} nwm --xephyr &
+
+# Oddly we have to wait for 
+#sleep 1
+
+
+DISPLAY=${STREAM_DISPLAY} gnome-terminal -x sh -c 'top; exec bash' &
+
+DISPLAY=${STREAM_DISPLAY} gnome-terminal -x sh -c 'vim; exec bash' &
+
+DISPLAY=${STREAM_DISPLAY} gnome-terminal -x sh -c 'irssi; exec bash' &
+
+DISPLAY=${STREAM_DISPLAY} google-chrome http://boards.4chan.org/tv/ &
