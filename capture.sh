236b1ed5779cc82424fb7b8484a72767e6fa6ea6
on-three <on.three.email@gmail.com>
2015-08-29 02:59:17 -0700

    Capture script

---
+++ capture.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#capture.sh
+#AUTHOR: on-three
+#DATE: Sat August 29th 2015
+#DESC: Startes a desktop capture piped from ffmpeg x11grab into a simulated
+# video capture device via kernel module v4l2loopback.
+# Make sure v4l2loopback kernel module is present (sudo modprobe v4l2loopback)
+
+
+ffmpeg -f x11grab -s 1280x720 -i :0.0+0,0 -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video0 & \
+sleep 4 ; vlc -I dummy v4l2:///dev/video0 --sout \
+"#transcode{vcodec=h264,vb=2200,vbv-maxrate=3000,vbv-bufsize=6000,scale=1,width=1280,height=720,venc=x264{aud,profile=high,level=30,keyint=30,ref=1},acodec=mpga,ab=128,channels=2,samplerate=44100}:http{mux=ts,dst=:8080/}" :sout-keep \
+&& fg
