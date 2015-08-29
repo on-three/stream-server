35d589ec65348b900862bb926c4a8dd40f4a067f
on-three <on.three.email@gmail.com>
2015-08-29 03:58:00 -0700

    added some configuration and (non working) sound.

--- capture.sh
+++ capture.sh
@@ -7,8 +7,13 @@
 # video capture device via kernel module v4l2loopback.
 # Make sure v4l2loopback kernel module is present (sudo modprobe v4l2loopback)
 
+SCREEN_WIDTH=1280
+SCREEN_HEIGHT=720
+SCREEN_LEFT=600
+SCREEN_TOP=150
 
-ffmpeg -f x11grab -s 1280x720 -i :0.0+0,0 -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video0 & \
-sleep 4 ; vlc -I dummy v4l2:///dev/video0 --sout \
-"#transcode{vcodec=h264,vb=2200,vbv-maxrate=3000,vbv-bufsize=6000,scale=1,width=1280,height=720,venc=x264{aud,profile=high,level=30,keyint=30,ref=1},acodec=mpga,ab=128,channels=2,samplerate=44100}:http{mux=ts,dst=:8080/}" :sout-keep \
+
+ffmpeg -f x11grab -s ${SCREEN_WIDTH}x${SCREEN_HEIGHT} -i :0.0+${SCREEN_LEFT},${SCREEN_TOP} -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video0 & \
+sleep 4 ; vlc -I dummy v4l2:///dev/video0 :v4l2-standard=ALL :input-slave=alsa://hw:0,0 :live-caching=300 --sout \
+"#transcode{vcodec=h264,vb=2200,vbv-maxrate=3000,vbv-bufsize=6000,scale=1,width=${SCREEN_WIDTH},height=${SCREEN_HEIGHT},venc=x264{aud,profile=high,level=30,keyint=30,ref=1},acodec=mpga,ab=128,channels=2,samplerate=44100}:http{mux=ts,dst=:8080/}" :sout-keep \
 && fg
